58f01f4747d8af6634ef5236a8eb05d8a38ee499
hibix43 <hibix43knzw+github@gmail.com>
2025-10-28 11:51:42 +0900

    WIP

--- test/mock_provider_test.dart
+++ test/mock_provider_test.dart
@@ -6,12 +6,20 @@ part 'mock_provider_test.g.dart';
 
 void main() {
   test('Minimum sample code to describe the problem', () {
+    // final notifier = SampleNotifierMock();
+
     final container = ProviderContainer.test(
+      // Not working
       overrides: [sampleProvider.overrideWith(() => SampleNotifierMock())],
+      // overrides: [sampleProvider.overrideWith(() => SampleNotifier2())],
+
+      // Pass the test by overriding the build method
+      // overrides: [sampleProvider.overrideWithBuild((_, _) => false)],
     );
 
     // Null check operator used on a null value
     container.read(sampleProvider.notifier);
+    // verify(notifier.build()).called(1);
   });
 }
 
@@ -26,3 +34,13 @@ class SampleNotifier extends _$SampleNotifier {
 class SampleNotifierMock extends $Notifier<bool>
     with Mock
     implements SampleNotifier {}
+
+class SampleNotifier2 extends $Notifier<bool> implements SampleNotifier {
+  @override
+  bool build() {
+    return false;
+  }
+
+  @override
+  void runBuild() {}
+}
