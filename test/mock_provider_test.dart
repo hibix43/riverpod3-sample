f6f5d31e6a31c6b93ee9b4677989e603cd01a8d8
hibix43 <hibix43knzw+github@gmail.com>
2025-10-30 17:59:54 +0900

    Fix mock provider implementation and add GitHub issue comment
    - Fix SampleNotifierMock to extend _ instead of <bool>
    - Add build() method implementation to the mock class
    - Add GitHub issue comment documentation for issue #4371
    - Resolve 'Null check operator used on a null value' error

--- test/mock_provider_test.dart
+++ test/mock_provider_test.dart
@@ -6,20 +6,13 @@ part 'mock_provider_test.g.dart';
 
 void main() {
   test('Minimum sample code to describe the problem', () {
-    // final notifier = SampleNotifierMock();
-
     final container = ProviderContainer.test(
       // Not working
       overrides: [sampleProvider.overrideWith(() => SampleNotifierMock())],
-      // overrides: [sampleProvider.overrideWith(() => SampleNotifier2())],
-
-      // Pass the test by overriding the build method
-      // overrides: [sampleProvider.overrideWithBuild((_, _) => false)],
     );
 
     // Null check operator used on a null value
     container.read(sampleProvider.notifier);
-    // verify(notifier.build()).called(1);
   });
 }
 
@@ -31,16 +24,17 @@ class SampleNotifier extends _$SampleNotifier {
   }
 }
 
-class SampleNotifierMock extends $Notifier<bool>
-    with Mock
-    implements SampleNotifier {}
+// Incorrect implementation!!
+// class SampleNotifierMock extends $Notifier<bool>
+//     with Mock
+//     implements SampleNotifier {}
 
-class SampleNotifier2 extends $Notifier<bool> implements SampleNotifier {
+// Correct implementation!!
+class SampleNotifierMock extends _$SampleNotifier
+    with Mock
+    implements SampleNotifier {
   @override
   bool build() {
     return false;
   }
-
-  @override
-  void runBuild() {}
 }
