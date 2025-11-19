378ebf93c590ab5f669f2d850cb9a13a6b25c393
hibix43 <hibix43knzw+github@gmail.com>
2025-11-19 11:24:51 +0900

    Add comprehensive tests for Notifier and AsyncNotifier overrides with mock implementations
    - Introduced multiple test cases for both Notifier and AsyncNotifier, covering successful and failed overrides with mock objects.
    - Added mock classes for Notifier and AsyncNotifier, including correct and incorrect implementations.
    - Ensured tests validate behavior when exceptions are thrown during the build process.

--- test/mock_provider_test.dart
+++ test/mock_provider_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart'
+    show Notifier, AsyncNotifier;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -5,14 +7,193 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'mock_provider_test.g.dart';
 
 void main() {
-  test('Minimum sample code to describe the problem', () {
-    final container = ProviderContainer.test(
-      // Not working
-      overrides: [sampleProvider.overrideWith(() => SampleNotifierMock())],
-    );
+  group('Notifier', () {
+    test('Failed override with mock', () {
+      final mock = SampleNotifierMockIncorrect();
+      when(mock.build()).thenAnswer((_) => true);
 
-    // Null check operator used on a null value
-    container.read(sampleProvider.notifier);
+      final container = ProviderContainer.test(
+        overrides: [sampleProvider.overrideWith(() => mock)],
+      );
+
+      // Null check operator used on a null value
+      final state = container.read(sampleProvider);
+      expect(state, true);
+    });
+
+    test('Successfully override with mock', () {
+      final mock = SampleNotifierMock();
+      when(mock.build()).thenAnswer((_) => true);
+
+      final container = ProviderContainer.test(
+        overrides: [sampleProvider.overrideWith(() => mock)],
+      );
+
+      final state = container.read(sampleProvider);
+      expect(state, true);
+    });
+
+    test('Successfully override with mock without generated code', () {
+      final mock = SampleNotifierMockWithoutGeneratedCode();
+      when(mock.build()).thenAnswer((_) => true);
+
+      final container = ProviderContainer.test(
+        overrides: [sampleProvider.overrideWith(() => mock)],
+      );
+
+      final state = container.read(sampleProvider);
+      expect(state, true);
+    });
+  });
+
+  group('AsyncNotifier', () {
+    test('Failed override with mock', () async {
+      final mock = SampleAsyncNotifierMockIncorrect();
+
+      when(mock.build()).thenAnswer((_) async => true);
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+      );
+
+      // Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.
+      final state = await container.read(sampleAsyncProvider.future);
+      expect(state, true);
+    });
+
+    test('Successfully override with mock', () async {
+      final mock = SampleAsyncNotifierMock();
+      when(mock.build()).thenAnswer((_) async => true);
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+      );
+
+      final state = await container.read(sampleAsyncProvider.future);
+      expect(state, true);
+    });
+
+    test('Successfully override with mock without generated code', () async {
+      final mock = SampleAsyncNotifierMockWithoutGeneratedCode();
+      when(mock.build()).thenAnswer((_) async => true);
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+      );
+
+      final state = await container.read(sampleAsyncProvider.future);
+      expect(state, true);
+    });
+  });
+
+  group('AsyncNotifier#build throws error', () {
+    test('Failed override with mock', () async {
+      final mock = SampleAsyncNotifierMockIncorrect();
+      when(mock.build()).thenAnswer((_) async => throw Exception('test'));
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+        retry: (retryCount, error) {
+          return null;
+        },
+      );
+
+      // retry を無効化してもしなくても以下のエラーが発生する
+      // Expected: throws <Instance of 'Exception'>
+      // Actual: <Instance of 'Future<bool>'>
+      //  Which: threw StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      await expectLater(
+        container.read(sampleAsyncProvider.future),
+        throwsA(isA<Exception>()),
+      );
+
+      // retry を無効化してもしなくても以下のエラーが発生する
+      // TimeoutException after 0:00:30.000000: Test timed out after 30 seconds. See https://pub.dev/packages/test#timeouts
+      // dart:isolate  _RawReceivePort._handleMessage
+      // Expected: throws <Instance of 'Exception'>
+      // Actual: <Instance of 'Future<bool>'>
+      // Which: threw StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      final sub = container.listen(
+        sampleAsyncProvider.future,
+        (previous, next) {},
+      );
+      await expectLater(sub.read(), throwsA(isA<Exception>()));
+      sub.close();
+    });
+
+    test('Successfully override with mock', () async {
+      final mock = SampleAsyncNotifierMock();
+      when(mock.build()).thenAnswer((_) async => throw Exception('test'));
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+        retry: (retryCount, error) {
+          return null;
+        },
+      );
+
+      // retry を無効化しないと以下のエラーが発生する
+      // Expected: throws <Instance of 'Exception'>
+      // Actual: <Instance of 'Future<bool>'>
+      //  Which: threw StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      await expectLater(
+        container.read(sampleAsyncProvider.future),
+        throwsA(isA<Exception>()),
+      );
+
+      // retry を無効化しないと以下のエラーが発生する
+      // TimeoutException after 0:00:30.000000: Test timed out after 30 seconds. See https://pub.dev/packages/test#timeouts
+      // dart:isolate  _RawReceivePort._handleMessage
+      // Expected: throws <Instance of 'Exception'>
+      // Actual: <Instance of 'Future<bool>'>
+      // Which: threw StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      final sub = container.listen(
+        sampleAsyncProvider.future,
+        (previous, next) {},
+      );
+      await expectLater(sub.read(), throwsA(isA<Exception>()));
+      sub.close();
+    });
+
+    test('Successfully override with mock without generated code', () async {
+      final mock = SampleAsyncNotifierMockWithoutGeneratedCode();
+      when(mock.build()).thenAnswer((_) async => throw Exception('test'));
+
+      final container = ProviderContainer.test(
+        overrides: [sampleAsyncProvider.overrideWith(() => mock)],
+        retry: (retryCount, error) {
+          return null;
+        },
+      );
+
+      // retry を無効化しないと以下のエラーが発生する
+      // StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      await expectLater(
+        container.read(sampleAsyncProvider.future),
+        throwsA(isA<Exception>()),
+      );
+
+      // retry を無効化しないと以下のエラーが発生する
+      // TimeoutException after 0:00:30.000000: Test timed out after 30 seconds.
+      // Expected: throws <Instance of 'Exception'> with `message`: 'Exception: test'
+      // Actual: <Instance of 'Future<bool>'>
+      // Which: threw StateError:<Bad state: The provider sampleAsyncProvider was disposed during loading state, yet no value could be emitted.>
+      final sub = container.listen(
+        sampleAsyncProvider.future,
+        (previous, next) {},
+      );
+      await expectLater(
+        sub.read(),
+        throwsA(
+          isA<Exception>().having(
+            (e) => e.toString(),
+            'message',
+            'Exception: test',
+          ),
+        ),
+      );
+      sub.close();
+    });
   });
 }
 
@@ -24,17 +205,104 @@ class SampleNotifier extends _$SampleNotifier {
   }
 }
 
-// Incorrect implementation!!
-// class SampleNotifierMock extends $Notifier<bool>
-//     with Mock
-//     implements SampleNotifier {}
-
 // Correct implementation!!
 class SampleNotifierMock extends _$SampleNotifier
     with Mock
     implements SampleNotifier {
   @override
   bool build() {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as bool;
+  }
+}
+
+// Incorrect implementation!!
+class SampleNotifierMockIncorrect extends $Notifier<bool>
+    with Mock
+    implements SampleNotifier {
+  @override
+  bool build() {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as bool;
+  }
+}
+
+// Correct implementation without generated code!!
+// Notifier クラスを利用するためには import 'package:flutter_riverpod/flutter_riverpod.dart'; が必要だった
+// しかし、VSCodeのquick fixでは import 'package:flutter_riverpod/flutter_riverpod.dart'; を追加してくれない
+// そのため、手動で追加する必要があった
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+class SampleNotifierMockWithoutGeneratedCode extends Notifier<bool>
+    with Mock
+    implements SampleNotifier {
+  @override
+  bool build() {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as bool;
+  }
+}
+
+@riverpod
+class SampleAsyncNotifier extends _$SampleAsyncNotifier {
+  @override
+  FutureOr<bool> build() async {
+    await Future.delayed(const Duration(milliseconds: 100));
     return false;
+  }
+}
+
+class SampleAsyncNotifierMock extends _$SampleAsyncNotifier
+    with Mock
+    implements SampleAsyncNotifier {
+  @override
+  FutureOr<bool> build() async {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as FutureOr<bool>;
+  }
+}
+
+@riverpod
+class SampleAsyncNotifierMockIncorrect extends $AsyncNotifier<bool>
+    with Mock
+    implements SampleAsyncNotifier {
+  @override
+  FutureOr<bool> build() async {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as FutureOr<bool>;
+  }
+}
+
+@riverpod
+class SampleAsyncNotifierMockWithoutGeneratedCode extends AsyncNotifier<bool>
+    with Mock
+    implements SampleAsyncNotifier {
+  @override
+  FutureOr<bool> build() async {
+    return noSuchMethod(
+          Invocation.method(#build, []),
+          returnValue: false,
+          returnValueForMissingStub: false,
+        )
+        as FutureOr<bool>;
   }
 }
