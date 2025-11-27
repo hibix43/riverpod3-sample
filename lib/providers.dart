eae811f260af99fc7c98419067a44709f7ebfd5a
hibix43 <hibix43knzw+github@gmail.com>
2025-11-27 19:06:31 +0900

    Refactor Repository and Driver classes for improved data fetching
    - Introduced a new `Driver` class with asynchronous API call capabilities.
    - Updated the `Repository` class to utilize the `Driver` for fetching data, renaming methods for clarity.
    - Modified service classes to call the new `fetchHelloWorld` method instead of the previous `fetchData`.
    - Adjusted test cases to reflect changes in method names and error handling, ensuring consistency with the new `ApiFetchDataError` exception.
    - Enhanced test coverage for service calculations, including retry logic and exception scenarios.

--- lib/providers.dart
+++ lib/providers.dart
@@ -3,10 +3,28 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'providers.g.dart';
 
-class Repository {
-  Future<String> fetchData() async {
-    await Future.delayed(const Duration(milliseconds: 500));
+class Driver {
+  const Driver._();
+
+  static Future<Driver> create() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return const Driver._();
+  }
+
+  Future<String> runApi() async {
+    await Future.delayed(const Duration(seconds: 1));
     return "hello";
+  }
+}
+
+class Repository {
+  final Driver driver;
+
+  Repository({required this.driver});
+
+  Future<String> fetchHelloWorld() async {
+    final data = await driver.runApi();
+    return "$data world";
   }
 }
 
@@ -20,13 +38,13 @@ class ServiceWithoutRef {
   });
 
   Future<void> calc() async {
-    final data = await repository.fetchData();
+    final data = await repository.fetchHelloWorld();
     runSideEffect(data);
   }
 
   Future<void> calcWithRetry({int retryCount = 0}) async {
     try {
-      final data = await repository.fetchData();
+      final data = await repository.fetchHelloWorld();
       runSideEffect(data);
     } catch (e) {
       if (retryCount < 3) {
@@ -46,42 +64,20 @@ class ServiceWithRef {
   const ServiceWithRef({required this.ref});
 
   Future<void> calc() async {
-    final repository = ref.read(repositoryProvider);
-    final data = await repository.fetchData();
+    final repository = await ref.read(repositoryProvider.future);
+    final data = await repository.fetchHelloWorld();
     ref.read(runSideEffectProvider)(data);
   }
 
   Future<void> calcWithRetry({int retryCount = 0}) async {
     try {
-      final repository = ref.read(repositoryProvider);
-      final data = await repository.fetchData();
+      final repository = await ref.read(repositoryProvider.future);
+      final data = await repository.fetchHelloWorld();
       ref.read(runSideEffectProvider)(data);
     } catch (e) {
       if (retryCount < 3) {
         await Future.delayed(const Duration(milliseconds: 500));
         await calcWithRetry(retryCount: retryCount + 1);
-      } else {
-        debugPrint('retry count is over 3 with error: $e');
-        rethrow;
-      }
-    }
-  }
-
-  Future<void> calcWithFuture() async {
-    final repository = await ref.read(repositoryForFutureProvider.future);
-    final data = await repository.fetchData();
-    debugPrint("side effect: $data");
-  }
-
-  Future<void> calcWithFutureWithRetry({int retryCount = 0}) async {
-    try {
-      final repository = await ref.read(repositoryForFutureProvider.future);
-      final data = await repository.fetchData();
-      debugPrint("side effect: $data");
-    } catch (e) {
-      if (retryCount < 3) {
-        await Future.delayed(const Duration(milliseconds: 500));
-        await calcWithFutureWithRetry(retryCount: retryCount + 1);
       } else {
         debugPrint('retry count is over 3 with error: $e');
         rethrow;
@@ -98,20 +94,21 @@ void Function(String) runSideEffect(Ref ref) {
 }
 
 @riverpod
-Repository repository(Ref ref) {
-  return Repository();
+Future<Driver> driver(Ref ref) {
+  return Driver.create();
 }
 
 @riverpod
-Future<Repository> repositoryForFuture(Ref ref) async {
-  await Future.delayed(const Duration(seconds: 1));
-  return Repository();
+Future<Repository> repository(Ref ref) async {
+  final driver = await ref.watch(driverProvider.future);
+  return Repository(driver: driver);
 }
 
 @riverpod
-ServiceWithoutRef serviceWithoutRef(Ref ref) {
+Future<ServiceWithoutRef> serviceWithoutRef(Ref ref) async {
+  final repository = await ref.watch(repositoryProvider.future);
   return ServiceWithoutRef(
-    repository: ref.watch(repositoryProvider),
+    repository: repository,
     runSideEffect: (data) {
       // UnmountedRefException (Cannot use the Ref of  after it has been disposed.
       ref.read(runSideEffectProvider)(data);
@@ -125,10 +122,12 @@ ServiceWithoutRef serviceWithoutRef(Ref ref) {
 }
 
 @riverpod
-ServiceWithoutRef serviceWithoutRef2(Ref ref) {
+Future<ServiceWithoutRef> serviceWithoutRef2(Ref ref) async {
+  final repository = await ref.watch(repositoryProvider.future);
+  // ref.read(serviceWithoutRef2Provider.future); としたら、UnmountedRefExceptionが発生する
   final runSideEffectFunction = ref.watch(runSideEffectProvider);
   return ServiceWithoutRef(
-    repository: ref.watch(repositoryProvider),
+    repository: repository,
     runSideEffect: runSideEffectFunction,
   );
 }
--- test/providers_test.dart
+++ test/providers_test.dart
@@ -7,115 +7,121 @@ void main() {
   group('ServiceWithoutRef', () {
     test('withoutRef_read_失敗', () async {
       final container = ProviderContainer.test();
-      final service = container.read(serviceWithoutRefProvider);
+      final service = await container.read(serviceWithoutRefProvider.future);
       await service.calc();
     });
 
     test('withoutRef_listen_成功', () async {
       final container = ProviderContainer.test();
       final sub = container.listen(
-        serviceWithoutRefProvider,
+        serviceWithoutRefProvider.future,
         (previous, next) {},
       );
-      final service = sub.read();
+      final service = await sub.read();
       await service.calc();
       sub.close();
     });
 
-    test('withoutRef2_read_成功', () async {
+    test('withoutRef2_read_失敗', () async {
       final container = ProviderContainer.test();
-      final service = container.read(serviceWithoutRef2Provider);
+      final service = await container.read(serviceWithoutRef2Provider.future);
       await service.calc();
     });
 
     test('withoutRef2_listen_成功', () async {
       final container = ProviderContainer.test();
       final sub = container.listen(
-        serviceWithoutRef2Provider,
+        serviceWithoutRef2Provider.future,
         (previous, next) {},
       );
-      final service = sub.read();
+      final service = await sub.read();
       await service.calc();
       sub.close();
     });
 
     test('withoutRef_read_成功_例外期待', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
-      final service = container.read(serviceWithoutRefProvider);
-      await expectLater(service.calc, throwsA(isA<FetchDataError>()));
+      final service = await container.read(serviceWithoutRefProvider.future);
+      await expectLater(service.calc, throwsA(isA<ApiFetchDataError>()));
     });
 
     test('withoutRef_listen_成功_例外期待', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final sub = container.listen(
-        serviceWithoutRefProvider,
+        serviceWithoutRefProvider.future,
         (previous, next) {},
       );
-      final service = sub.read();
-      await expectLater(service.calc(), throwsA(isA<FetchDataError>()));
+      final service = await sub.read();
+      await expectLater(service.calc(), throwsA(isA<ApiFetchDataError>()));
       sub.close();
     });
 
     test('withoutRef2_listen_成功_例外期待', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final sub = container.listen(
-        serviceWithoutRef2Provider,
+        serviceWithoutRef2Provider.future,
         (previous, next) {},
       );
-      final service = sub.read();
-      await expectLater(service.calc(), throwsA(isA<FetchDataError>()));
+      final service = await sub.read();
+      await expectLater(service.calc(), throwsA(isA<ApiFetchDataError>()));
       sub.close();
     });
 
     test('withoutRef_read_成功_例外期待_リトライ', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
-      final service = container.read(serviceWithoutRefProvider);
-      await expectLater(service.calcWithRetry, throwsA(isA<FetchDataError>()));
+      final service = await container.read(serviceWithoutRefProvider.future);
+      await expectLater(
+        service.calcWithRetry,
+        throwsA(isA<ApiFetchDataError>()),
+      );
     });
 
     test('withoutRef_listen_成功_例外期待_リトライ', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final sub = container.listen(
-        serviceWithoutRefProvider,
+        serviceWithoutRefProvider.future,
         (previous, next) {},
       );
-      final service = sub.read();
+      final service = await sub.read();
       await expectLater(
         service.calcWithRetry(),
-        throwsA(isA<FetchDataError>()),
+        throwsA(isA<ApiFetchDataError>()),
       );
       sub.close();
     });
 
     test('withoutRef2_read_成功_例外期待_リトライ', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
-      final service = container.read(serviceWithoutRef2Provider);
-      await expectLater(service.calcWithRetry, throwsA(isA<FetchDataError>()));
+      final service = await container.read(serviceWithoutRef2Provider.future);
+      await expectLater(
+        service.calcWithRetry,
+        throwsA(isA<ApiFetchDataError>()),
+      );
     });
   });
 
   test('withoutRef2_listen_成功_例外期待_リトライ', () async {
     final container = ProviderContainer.test(
-      overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+      overrides: [driverProvider.overrideWith((r) => MockDriver())],
     );
     final sub = container.listen(
-      serviceWithoutRef2Provider,
+      serviceWithoutRef2Provider.future,
       (previous, next) {},
     );
-    final service = sub.read();
-    await expectLater(service.calcWithRetry, throwsA(isA<FetchDataError>()));
+    final service = await sub.read();
+    await expectLater(service.calcWithRetry, throwsA(isA<ApiFetchDataError>()));
     sub.close();
   });
 
@@ -136,19 +142,19 @@ void main() {
 
     test('withRef_read_成功_例外期待', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final service = container.read(serviceWithRefProvider);
-      await expectLater(service.calc, throwsA(isA<FetchDataError>()));
+      await expectLater(service.calc, throwsA(isA<ApiFetchDataError>()));
     });
 
     test('withRef_listen_成功_例外期待', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final sub = container.listen(serviceWithRefProvider, (previous, next) {});
       final service = sub.read();
-      await expectLater(service.calc, throwsA(isA<FetchDataError>()));
+      await expectLater(service.calc, throwsA(isA<ApiFetchDataError>()));
       sub.close();
     });
 
@@ -157,95 +163,37 @@ void main() {
     // →非同期処理の時間がautoDisposeによるRefの破棄より長い場合に失敗すると言えそう
     test('withRef_read_失敗_例外期待_リトライ', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final service = container.read(serviceWithRefProvider);
-      await expectLater(service.calcWithRetry, throwsA(isA<FetchDataError>()));
+      await expectLater(
+        service.calcWithRetry,
+        throwsA(isA<ApiFetchDataError>()),
+      );
     });
 
     test('withRef_listen_成功_例外期待_リトライ', () async {
       final container = ProviderContainer.test(
-        overrides: [repositoryProvider.overrideWith((r) => MockRepository())],
-      );
-      final sub = container.listen(serviceWithRefProvider, (previous, next) {});
-      final service = sub.read();
-      await expectLater(service.calcWithRetry, throwsA(isA<FetchDataError>()));
-      sub.close();
-    });
-
-    test('withRef_read_成功_Future期待', () async {
-      final container = ProviderContainer.test();
-      final service = container.read(serviceWithRefProvider);
-      await service.calcWithFuture();
-    });
-
-    test('withRef_listen_成功_Future期待', () async {
-      final container = ProviderContainer.test();
-      final sub = container.listen(serviceWithRefProvider, (previous, next) {});
-      final service = sub.read();
-      await service.calcWithFuture();
-      sub.close();
-    });
-
-    test('withRef_read_成功_Future期待_例外期待', () async {
-      final container = ProviderContainer.test(
-        overrides: [
-          repositoryForFutureProvider.overrideWith((r) => MockRepository()),
-        ],
-      );
-      final service = container.read(serviceWithRefProvider);
-      await expectLater(service.calcWithFuture, throwsA(isA<FetchDataError>()));
-    });
-
-    test('withRef_listen_成功_Future期待_例外期待', () async {
-      final container = ProviderContainer.test(
-        overrides: [
-          repositoryForFutureProvider.overrideWith((r) => MockRepository()),
-        ],
-      );
-      final sub = container.listen(serviceWithRefProvider, (previous, next) {});
-      final service = sub.read();
-      await expectLater(service.calcWithFuture, throwsA(isA<FetchDataError>()));
-      sub.close();
-    });
-
-    test('withRef_read_失敗_Future期待_例外期待_リトライ', () async {
-      final container = ProviderContainer.test(
-        overrides: [
-          repositoryForFutureProvider.overrideWith((r) => MockRepository()),
-        ],
-      );
-      final service = container.read(serviceWithRefProvider);
-      await expectLater(
-        service.calcWithFutureWithRetry,
-        throwsA(isA<FetchDataError>()),
-      );
-    });
-
-    test('withRef_listen_成功_Future期待_例外期待_リトライ', () async {
-      final container = ProviderContainer.test(
-        overrides: [
-          repositoryForFutureProvider.overrideWith((r) => MockRepository()),
-        ],
+        overrides: [driverProvider.overrideWith((r) => MockDriver())],
       );
       final sub = container.listen(serviceWithRefProvider, (previous, next) {});
       final service = sub.read();
       await expectLater(
-        service.calcWithFutureWithRetry,
-        throwsA(isA<FetchDataError>()),
+        service.calcWithRetry,
+        throwsA(isA<ApiFetchDataError>()),
       );
       sub.close();
     });
   });
 }
 
-class MockRepository extends Mock implements Repository {
+class MockDriver extends Mock implements Driver {
   @override
-  Future<String> fetchData() async {
-    throw FetchDataError();
+  Future<String> runApi() async {
+    throw ApiFetchDataError();
   }
 }
 
-class FetchDataError extends Error {
-  FetchDataError();
+class ApiFetchDataError extends Error {
+  ApiFetchDataError();
 }
